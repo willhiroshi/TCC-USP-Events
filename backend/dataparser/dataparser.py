4b18ca1e002a7164ff131ec55c7806a1f926da1a
Willian Hiroshi Takihi <willhiroshi@hotmail.com>
2023-05-27 18:23:53 -0300

    Create dataparser to run Vicuna model locally

---
+++ backend/dataparser/dataparser.py
@@ -0,0 +1,21 @@
+import json
+
+from llama_cpp import Llama
+
+MODEL_PATH = (
+    "/home/hiroshi/eps/TCC-USP-Events/backend/models/ggml-vic13b-uncensored-q5_1.bin"
+)
+llm = Llama(model_path=MODEL_PATH, n_threads=4)
+
+TEMPERATURE = 0.70
+TOP_P = 0.95
+
+
+def process_post(post_text: str):
+    get_info = "Colete informações sobre o evento acima em formato JSON: data, localização, e preço ?\n\n"
+    text_to_be_processed = ". ".join([post_text, get_info])
+
+    output = llm(text_to_be_processed, temperature=TEMPERATURE, top_p=TOP_P)
+
+    processed_text = output["choices"][0]["text"].strip()
+    return json.loads(processed_text)
--- backend/webscraper/seleniumFaceScraper.py
+++ backend/webscraper/seleniumFaceScraper.py
@@ -13,58 +13,64 @@ chrome_options.add_argument("--headless")  # Run Chrome in headless mode
 CHROMEDRIVER_PATH = "./chromedriver/chromedriver"
 driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=chrome_options)
 
-# open site
-FACEBOOK_SITE = "https://www.facebook.com/ProReitoriadeCulturaeExtensao"
-driver.get(FACEBOOK_SITE)
 
-# close the login modal
-time.sleep(1)
-try:
-    close_button = driver.find_element(by=By.XPATH, value='//div[@aria-label="Fechar"]')
-    close_button.click()
-except:
-    print("Close button not found. Skipping click.")
+def get_facebook_posts(facebook_page: str, num_posts: int = 5):
+    # open site
+    driver.get(facebook_page)
 
-# get limited number of posts
-LIMIT_POSTS = 5
-posts_content = set()
-
-while len(posts_content) <= LIMIT_POSTS:
-    # click on 'Ver mais' buttons available
+    # close the login modal
+    time.sleep(1)
     try:
-        ver_mais_buttons = driver.find_elements(
-            By.XPATH,
-            f'//*[text()="Ver mais"]',
+        close_button = driver.find_element(
+            by=By.XPATH, value='//div[@aria-label="Fechar"]'
         )
-        for button in ver_mais_buttons:
-            driver.execute_script("arguments[0].click();", button)
-            time.sleep(1)  # Wait for the expanded text to load
-    except Exception as error:
-        print(f"Error clicking on 'Ver mais' button. ERROR=[{error}]")
+        close_button.click()
+    except:
+        print("Close button not found. Skipping click.")
 
-    # find all posts
-    page_source = driver.page_source
-    soup = BeautifulSoup(page_source, "html.parser")
-    all_posts = soup.find_all("div", {"class": "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"})
-
-    # get post content information
-    for post in all_posts:
+    # get limited number of posts
+    posts_content = set()
+    reach_maximum_posts = False
+    while not reach_maximum_posts:
+        # click on 'Ver mais' buttons available
         try:
-            post_text = post.find(
-                "span",
-                {
-                    "class": "x193iq5w xeuugli x13faqbe x1vvkbs x10flsy6 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x41vudc x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h"
-                },
+            ver_mais_buttons = driver.find_elements(
+                By.XPATH,
+                f'//*[text()="Ver mais"]',
             )
-
-            posts_content.add(post_text.text)
+            for button in ver_mais_buttons:
+                driver.execute_script("arguments[0].click();", button)
+                time.sleep(1)  # Wait for the expanded text to load
         except Exception as error:
-            print("Did not find post's text")
+            print(f"Error clicking on 'Ver mais' button. ERROR=[{error}]")
 
-    # scroll down on page
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(3)
+        # find all posts
+        page_source = driver.page_source
+        soup = BeautifulSoup(page_source, "html.parser")
+        all_posts = soup.find_all(
+            "div", {"class": "x1yztbdb x1n2onr6 xh8yej3 x1ja2u2z"}
+        )
 
-for index, post in enumerate(posts_content):
-    print(f"Index: {index}, Post: {post} \n")
-driver.quit()
+        # get post content information
+        for post in all_posts:
+            try:
+                post_text = post.find(
+                    "span",
+                    {
+                        "class": "x193iq5w xeuugli x13faqbe x1vvkbs x10flsy6 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x x4zkp8e x41vudc x6prxxf xvq8zen xo1l8bm xzsf02u x1yc453h"
+                    },
+                )
+                posts_content.add(post_text.text)
+                if len(posts_content) >= num_posts:
+                    reach_maximum_posts = True
+                    break
+            except Exception as error:
+                print("Did not find post's text")
+
+        # scroll down on page
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(3)
+
+    driver.quit()
+
+    return posts_content
