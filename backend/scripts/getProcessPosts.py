dd309605c05c34e7a6a42a47c9e74296a860f39c
Willian Hiroshi <49212715+willhiroshi@users.noreply.github.com>
2023-05-30 14:48:54 -0300

    Create dataparser to run Vicuna model locally (#5) (#7)

--- backend/scripts/getProcessPosts.py
+++ backend/scripts/getProcessPosts.py
@@ -1,5 +1,5 @@
-from webscraper.seleniumFaceScraper import get_facebook_posts
-from dataparser.dataparser import process_post
+from dataparser import process_post
+from faceScraper import get_facebook_posts
 
 facebook_page = "https://www.facebook.com/ProReitoriadeCulturaeExtensao"
 posts = get_facebook_posts(facebook_page=facebook_page, num_posts=2)
@@ -7,5 +7,3 @@ posts = get_facebook_posts(facebook_page=facebook_page, num_posts=2)
 processed_posts = []
 for post in posts:
     processed_posts.append(process_post(post))
-
-# print(processed_posts)
