29ef3062bb20bc36e1b908035a4f7dea8a50d396
Willian Hiroshi Takihi <willhiroshi@hotmail.com>
2023-08-10 21:24:18 -0300

    Add react-query-dev-tools and handle errors when fetching events

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import HomePage from './components/HomePage/HomePage';
 import './index.css';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 const queryClient = new QueryClient();
+
 root.render(
   <QueryClientProvider client={queryClient}>
     <HomePage />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 );
