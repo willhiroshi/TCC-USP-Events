b33e5b89f031cc6c560b170394388dab89493683
Willian Hiroshi <49212715+willhiroshi@users.noreply.github.com>
2023-10-02 22:33:35 -0300

    Get events by type parameter (#30)

--- backend/server/models.py
+++ backend/server/models.py
@@ -2,10 +2,17 @@ from django.db import models
 
 
 class Event(models.Model):
+    EVENT_TYPES = [
+        ("unclassified", "Unclassified"),
+        ("culture", "Culture"),
+        ("sport", "Sport"),
+    ]
+
+    hash_id = models.TextField(primary_key=True, default="dftValue")
     post_link = models.TextField(null=False)
     date = models.CharField(max_length=50, null=True)
     address = models.CharField(max_length=300, null=True)
     price = models.CharField(max_length=50, null=True)
     lat = models.CharField(max_length=25, null=True)
     lng = models.CharField(max_length=25, null=True)
-    hash_id = models.TextField(primary_key=True, default='dftValue')
+    type = models.CharField(max_length=40, choices=EVENT_TYPES, default="unclassified")
--- backend/server/views.py
+++ backend/server/views.py
@@ -11,34 +11,53 @@ from .serializers import EventSerializer
 
 @api_view(["GET", "POST", "DELETE"])
 def event(request):
+    START_DATE_PARAM = "start_date"
+    END_DATE_PARAM = "end_date"
+    TYPES_PARAM = "types"
+
     if request.method == "GET":
         queryParams = request.query_params
+        events = Event.objects.all()
 
-        if 'start_date' in queryParams and 'end_date' in queryParams:
-            start = queryParams['start_date']
-            end = queryParams['end_date']
+        if START_DATE_PARAM in queryParams and END_DATE_PARAM in queryParams:
+            start = queryParams[START_DATE_PARAM]
+            end = queryParams[END_DATE_PARAM]
+            events = events.filter(date__gte=start, date__lte=end)
 
-            events = Event.objects.filter(date__gte=start, date__lte=end)
-        else:
-            events = Event.objects.all()
+        if TYPES_PARAM in queryParams:
+            requested_types = queryParams[TYPES_PARAM].split(",")
+
+            invalid_types = [type for type in requested_types if type not in dict(Event.EVENT_TYPES).keys()]
+            if invalid_types:
+                return Response(
+                    {"error": f"Invalid event type(s): {', '.join(invalid_types)}"},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            events = events.filter(type__in=requested_types)
+
         serializer = EventSerializer(events, many=True)
 
         return Response({"data": serializer.data})
 
     elif request.method == "POST":
+        # create a hash id for the post
         hash_object = hashlib.sha256()
-        post_link:str = request.data['post_link']
+        post_link: str = request.data["post_link"]
         hash_object.update(post_link.encode())
         hash_hex = hash_object.hexdigest()
         dataToSerialize = request.data
-        dataToSerialize['hash_id'] = hash_hex
-        serializer = EventSerializer(data=request.data)
+        dataToSerialize["hash_id"] = hash_hex
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({"data": "Error to save Post", "Post": dataToSerialize}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = EventSerializer(data=request.data)
+        if not serializer.is_valid():
+            return Response(
+                {"error": serializer.errors},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        serializer.save()
+        return Response({"data": serializer.data}, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -57,7 +76,9 @@ def event_detail(request, hash_id):
         if serializer.is_valid():
             serializer.save()
             return Response({"data": serializer.data})
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"error": serializer.errors}, status=status.HTTP_400_BAD_REQUEST
+        )
 
     elif request.method == "DELETE":
         event.delete()
