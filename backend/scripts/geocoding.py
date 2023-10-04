d1089617d6b81a4469e2d81a64d84d897c192be1
Willian Hiroshi Takihi <willhiroshi@hotmail.com>
2023-10-04 20:49:12 -0300

    Fix logger error on get coordinates function

--- backend/scripts/geocoding.py
+++ backend/scripts/geocoding.py
@@ -3,17 +3,19 @@ from geopy.geocoders import Nominatim
 
 APP_NAME = "TCC-USP-EVENTS"
 
+logger = Logger(__name__)
+
 
 def get_lat_lon_by_address(address: str) -> tuple[str, str]:
     try:
         geolocator = Nominatim(user_agent=APP_NAME)
         location = geolocator.geocode(address, country_codes="br")
-        Logger.info(f"Coords for address {address} obtained successfully - {location}")
+        logger.info(f"Coords for address {address} obtained successfully - {location}")
         if location == None:
             return None, None
 
     except Exception as error:
-        Logger.error(
+        logger.error(
             f"Not able to obtain the coords from address {address}. Default address used instead. Error=[{error}]"
         )
         address = "USP - Cidade Universitária"
