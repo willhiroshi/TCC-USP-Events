bb13dbcdb1fe939841f705f40bb8bec336391ab3
Willian Hiroshi Takihi <willhiroshi@hotmail.com>
2023-10-05 21:56:02 -0300

    Change React Icon and modify CollapsbleSidebar styles

---
+++ frontend/src/components/CollapsableSidebar/styles.ts
@@ -0,0 +1,22 @@
+const styles = {
+  sidebarButton: (isSelected: boolean) => ({
+    minHeight: 48,
+    justifyContent: 'initial',
+    pl: 3,
+    gap: '5px',
+    backgroundColor: isSelected ? 'rgba(0, 0, 0, 0.10)' : 'transparent',
+    ':hover': {
+      backgroundColor: 'rgba(0, 0, 0, 0.10)'
+    }
+  }),
+  sidebarIcon: {
+    minWidth: 0,
+    justifyContent: 'center'
+  },
+  sidebarButtonText: {
+    color: 'black',
+    fontSize: '12px'
+  }
+};
+
+export default styles;
