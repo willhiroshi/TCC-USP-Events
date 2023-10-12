c87db233611140a9acf401cb18d4da61ecd2bb57
Willian Hiroshi Takihi <willhiroshi@hotmail.com>
2023-10-12 18:33:33 -0300

    Implement private routes for logged in users

--- frontend/src/components/App/App.tsx
+++ frontend/src/components/App/App.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './styles';
 import HomePage from '../HomePage/HomePage';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useNavigate } from 'react-router-dom';
 import LocationlessPostsGrid from '../PostsGrid/PostsGrid';
 import CollapsableSidebar from '../CollapsableSidebar/CollapsableSidebar';
 import { Box, CssBaseline } from '@mui/material';
 import ProfilePage from '../ProfilePage/ProfilePage';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
+  const RedirectToRoot = () => {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+      navigate('/');
+    }, [navigate]);
+
+    return null;
+  };
+
   return (
     <Box sx={styles.wrapper}>
       <CssBaseline />
@@ -15,9 +26,10 @@ function App() {
         <CollapsableSidebar />
         <Box flexGrow={1}>
           <Routes>
+            <Route path="/*" element={<RedirectToRoot />} />
             <Route path="/" element={<HomePage />} />
             <Route path="/locationless" element={<LocationlessPostsGrid />} />
-            <Route path="/profile" element={<ProfilePage />} />
+            <Route path="/profile" element={<PrivateRoute Component={ProfilePage} />} />
           </Routes>
         </Box>
       </Box>
